489b37524ebb3ead70620849ada76fb9a1026c9c
Kerry Buckley <kerryjbuckley@gmail.com>
2021-04-28 21:34:55 +0100

    Shift subsequent chars based on the first one

--- lib/shifting_shift.ex
+++ lib/shifting_shift.ex
@@ -1,5 +1,21 @@
 defmodule ShiftingShift do
   def decrypt(text) do
-    text
+    text |> String.to_charlist() |> decrypt_chars() |> to_string
+  end
+
+  defp decrypt_chars([head | tail]) do
+    [head | decrypt_chars(tail, offset_for_letter(head))]
+  end
+
+  defp decrypt_chars([], _offset) do
+    []
+  end
+
+  defp decrypt_chars([head | tail], offset) do
+    [head - offset | decrypt_chars(tail, offset)]
+  end
+
+  defp offset_for_letter(letter) do
+    letter - ?A + 1
   end
 end
--- test/shifting_shift_test.exs
+++ test/shifting_shift_test.exs
@@ -6,5 +6,9 @@ defmodule ShiftingShiftTest do
     test "returns the first letter unchanged" do
       assert ShiftingShift.decrypt("A") == "A"
     end
+
+    test "shifts subsequent letters backwards by the number value of the first letter" do
+      assert ShiftingShift.decrypt("EMP") == "EHK"
+    end
   end
 end
